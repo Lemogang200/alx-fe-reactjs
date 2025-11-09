a72b66121a40f9f1317ea524b377e53113b9f3c8
Lemogang <taaiboseroka@gmail.com>
2025-11-09 20:32:07 +0200

    Changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './Header.jsx'
-import MainContent from './MainContent.jsx'
-import Footer from './Footer.jsx'
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx';
 
 function App() {
@@ -13,14 +13,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <WelcomeMessage />
       <Header /> 
       <MainContent />
       <Footer />
@@ -32,19 +25,6 @@ function App() {
       name="Alice" 
       age="25" 
       bio="Loves hiking and photography" />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <WelcomeMessage />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- alx-react-app/src/main.jsx
+++ alx-react-app/src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './Header.jsx'
-import MainContent from './MainContent.jsx'
-import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Header /> 
-    <MainContent />
-    <Footer />
   </StrictMode>,
 )
