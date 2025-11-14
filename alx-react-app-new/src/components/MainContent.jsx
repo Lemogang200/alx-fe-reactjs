bf507be59a22cd55544873f8878dea23f62327df
Lemogang <taaiboseroka@gmail.com>
2025-11-14 11:26:09 +0200

    New app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+        <div>
+            <main style={{ backgroundColor: 'grey', color: 'white', textAlign: 'center' }}>
+                <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        </div>
+    );
+}
+
+export default MainContent;
