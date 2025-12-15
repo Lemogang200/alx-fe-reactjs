1edd4927b586c98a393b6281c69e30d0ccd08a64
Lemogang <taaiboseroka@gmail.com>
2025-12-15 18:04:54 +0200

    added

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h2 className="text-3xl font-bold text-blue-500">Tailwind v4 is working</h2>
+  )
+}
+
+export default App
