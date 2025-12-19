3f95a886b444c30bfedebb6a2cce282919a10f64
Lemogang <taaiboseroka@gmail.com>
2025-12-19 12:17:24 +0200

    update

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
@@ -9,35 +9,37 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div className="p-6">
-      <nav className="space-x-4 mb-6">
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/posts/1">Post 1</Link>
-      </nav>
+    <BrowserRouter>
+      <div className="p-6">
+        <nav className="space-x-4 mb-6">
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/posts/1">Post 1</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* ✅ Protected Route */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          {/* ✅ Nested Routes */}
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+          {/* ✅ Protected Route */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          >
+            {/* ✅ Nested Routes */}
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
 
-        {/* ✅ Dynamic Route */}
-        <Route path="/posts/:id" element={<Post />} />
-      </Routes>
-    </div>
+          {/* ✅ Dynamic Route */}
+          <Route path="/posts/:id" element={<Post />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
