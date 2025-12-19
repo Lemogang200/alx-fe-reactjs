3602983191a755fea942c084602856244460c03b
Lemogang <taaiboseroka@gmail.com>
2025-12-19 10:11:39 +0200

    updated

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -19,7 +19,7 @@ export default function RecipeDetail() {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="max-w-4xl mx-auto p-6 shadow-lg bg-white rounded-lg">
       <Link
         to="/"
         className="text-blue-500 hover:underline mb-4 inline-block"
